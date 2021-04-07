365449842100631a900d480ad4e59bf2e9707223
phcs971 <48731731+phcs971@users.noreply.github.com>
2021-04-07 17:30:25 -0300

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,12 @@
+//: A SpriteKit based Playground
+
+import PlaygroundSupport
+import SpriteKit
+import SceneKit
+import ARKit
+
+print("<Main> Start")
+let viewController = ViewController();
+
+//print("<Main> View Controller Created")
+PlaygroundSupport.PlaygroundPage.current.liveView = viewController;
